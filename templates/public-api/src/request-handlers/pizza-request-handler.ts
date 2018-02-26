e32463f5fe20903d04857eb9383f15af0eb08e88
Aaron Greenspan <afg419@gmail.com>
2018-02-26 15:49:08 -0700

    somehow all pizza files were deleted

---
+++ templates/public-api/src/request-handlers/pizza-request-handler.ts
@@ -0,0 +1,35 @@
+import { PizzaLogic } from '../logic/pizza-logic'
+import { CreatePizzaRequest, CreatePizzaResponse, GetPizzaRequest, GetPizzaResponse } from '../endpoints/endpoint-types'
+import { mapPizzaToApiPizza } from '../utility/mapping-helper'
+import { Unauthorized, Request } from 'vineyard-lawn'
+
+export interface PizzaRequestHandler {
+  createPizza: (req: CreatePizzaRequest) => Promise<CreatePizzaResponse>
+  getPizza: (req: GetPizzaRequest) => Promise<GetPizzaResponse>
+}
+
+export function configurePizzaRequestHandler (pizzaLogic: PizzaLogic): PizzaRequestHandler {
+  return {
+    createPizza : (req: Request) => createPizza(pizzaLogic, req),
+    getPizza : (req: Request) => getPizza(pizzaLogic, req)
+  }
+}
+
+export const createPizza: (pizzaLogic: PizzaLogic, req: CreatePizzaRequest) => Promise<CreatePizzaResponse> =
+  async (pizzaLogic, req) => {
+    const createdPizza = await pizzaLogic.createPizza(req.data)
+    return mapPizzaToApiPizza(createdPizza)
+  }
+
+export const getPizza: (pizzaLogic: PizzaLogic, req: GetPizzaRequest) => Promise<GetPizzaResponse> =
+  async (pizzaLogic, req) => {
+    const retrievedPizza = await pizzaLogic.getPizza(req.data.pizzaId)
+    return mapPizzaToApiPizza(retrievedPizza)
+  }
+
+// Auth is done at the request handler level
+// export function getLoggedInUserId (req: Request): string {
+//   const userId: string = req.session.user
+//   if (userId) return userId
+//   throw new Unauthorized()
+// }
---
+++ templates/public-api/test/full-automated/http/pizza/pizza-test.ts
@@ -0,0 +1,58 @@
+import * as assert from 'assert'
+const request = require('request-promise')
+import { defaultVillage } from '../../../setup/server-setup'
+import { startApiServer } from '../../../../src/api/api-server'
+import { Server } from 'vineyard-lawn/source/server'
+import { PizzaType } from '../../../../src/model/model-types'
+import { assertEqualByData } from '../../../helpers/custom-assertions'
+import { randomCreatePizzaData } from '../../../../fixtures/random-types'
+
+describe('pizza end 2 end', function () {
+  const { village } = defaultVillage
+  let server: Server
+  let rootUrl: string
+
+  before(async () => {
+    server = await startApiServer(village)
+    rootUrl = `http://localhost:${server.getPort()}/v1/`
+  })
+
+  after(async () => {
+    server.stop()
+  })
+
+  it('creates a pizza', async function () {
+    const pizzaToCreate = randomCreatePizzaData()
+    const createPizzaOptions = {
+      method: 'POST',
+      uri: rootUrl + `pizza`,
+      json: true,
+      qs: {},
+      body: pizzaToCreate
+    }
+    const createdPizza = await request(createPizzaOptions)
+    assertEqualByData(pizzaToCreate, createdPizza, ['price'])
+    assert(createdPizza.price)
+    assert(createdPizza.id)
+  })
+
+  it('gets a pizza', async function () {
+    const pizzaToCreate = randomCreatePizzaData()
+    const createPizzaOptions = {
+      method: 'POST',
+      uri: rootUrl + `pizza`,
+      json: true,
+      body: pizzaToCreate
+    }
+    const createdPizza = await request(createPizzaOptions)
+
+    const getPizzaOptions = {
+      method: 'GET',
+      uri: rootUrl + `pizza`,
+      json: true,
+      qs: { pizzaId: createdPizza.id }
+    }
+    const retrievedPizza = await request(getPizzaOptions)
+    assertEqualByData(retrievedPizza, createdPizza)
+  })
+})
