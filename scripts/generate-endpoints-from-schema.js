7d45f82c4d1892df6fd37e26b3bdb62f6feed0ec
Aaron Greenspan <afg419@gmail.com>
2018-05-22 14:28:17 -0600

    adds startup script js

---
+++ scripts/generate-endpoints-from-schema.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vineyard_janus_1 = require("vineyard-janus");
+const config_1 = require("../config/config");
+const path = require("path");
+const schemaValidators = require('../src/endpoints/schema-validators.json');
+const wd = path.join(__dirname, '..', 'src');
+const { sourceDir, targetDir } = config_1.realConfig.janusEndpoints;
+const generator = vineyard_janus_1.configureJsonSchemaGeneration(wd + targetDir, wd + sourceDir, schemaValidators);
+generator.compileAll();
+//# sourceMappingURL=generate-endpoints-from-schema.js.map
