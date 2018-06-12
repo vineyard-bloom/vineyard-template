b1c675eb69e1f4d71f00b3b61433e231827ec80e
Patrick Medaugh <patrickmedaugh@gmail.com>
2018-06-12 15:07:44 -0600

    Removed more pizzas. Endpoint initializer returned to pre-janus

--- src/endpoints/endpoint-initializer.ts
+++ src/endpoints/endpoint-initializer.ts
@@ -1,22 +1,12 @@
-import { Request, Server } from 'vineyard-lawn'
+import { Method, Request, Server } from 'vineyard-lawn'
 import { Village } from '../village'
-import {
-  createEndpointToServeEndpointSchema,
-  pullEndpointDefinitionsFromSchema,
-  synthesizeApiActions
-} from './janus-lawn-helpers'
 
 export function initializeEndpoints (server: Server, village: Village) {
   const emptyPreprocessor = (request: Request) => Promise.resolve(request)
-  const { apiContract, config } = village
 
-  const { endpointDefinitions, rawSchema } = pullEndpointDefinitionsFromSchema(config.janusEndpoints)
-  const endpointInfos = synthesizeApiActions(apiContract, endpointDefinitions)
+  const validators = server.compileApiSchema(require('../endpoints/request-validation.json'))
 
-  if(config.janusEndpoints.endpointForSchema) {
-    endpointInfos.push(createEndpointToServeEndpointSchema(config.janusEndpoints.endpointForSchema, rawSchema))
-  }
-
-  server.compileApiSchema({})
-  server.createEndpoints(emptyPreprocessor, endpointInfos)
+  server.createEndpoints(emptyPreprocessor, [
+    
+  ])
 }
