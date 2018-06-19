6321d2e29226580ba9c224bc81714bcddec22276
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-06-19 13:48:30 -0600

    Moving shared files into common/files/src

--- generators/app/templates/lawn/files/src/backing-services.ts
+++
@@ -1,9 +0,0 @@
-import { FullConfig } from '../config/config-types'
-
-export function createBackingServices (config: FullConfig, overrides: Partial<BackingServices>): BackingServices {
-  return {}
-}
-
-export interface BackingServices {
-
-}
