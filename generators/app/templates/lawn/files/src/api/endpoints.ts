f12566fff1f21af6d986ac404a57dc3b9174202f
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-06-20 15:02:57 -0600

    Outlining health check endpoints

--- generators/app/templates/lawn/files/src/api/endpoints.ts
+++ generators/app/templates/lawn/files/src/api/endpoints.ts
@@ -9,6 +9,21 @@ export function initializeEndpoints (server: Server, village: Village) {
       method: Method.get,
       path: '/',
       action: () => Promise.resolve("hello")
+    }, {
+      method: Method.get,
+      path: '/status/shallow',
+      action: () => Promise.resolve({ status: 200, message: 'ok' })
+    }, {
+      method: Method.get,
+      path: '/status/deep',
+      action: () => {
+        try {
+          village.model.query('SELECT 100 as test')
+        } catch {
+          return Promise.resolve({ status: 500, message: 'vineyard-ground db is unreachable' })
+        }
+        return Promise.resolve({ status: 200, message: 'ok' })
+      }
     }
   ])
 }
