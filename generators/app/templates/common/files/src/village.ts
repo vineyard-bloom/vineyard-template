6ea71ee9ca460fe7eff7049061a73daacee2c864
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-06-20 15:25:38 -0600

    Adding script for sample table generation

--- generators/app/templates/common/files/src/village.ts
+++ generators/app/templates/common/files/src/village.ts
@@ -6,6 +6,7 @@ import {
   createBackingServices
 } from './backing-services'
 import { config as defaultConfig } from '../config/config'
+import { Schema } from 'vineyard-ground/source/schema';
 
 export interface Village {
   config: FullConfig
@@ -14,12 +15,25 @@ export interface Village {
   logic: Logic
 }
 
+const sampleSchema = new Schema({
+  samplePeople: {
+    properties: {
+      name: {
+        type: "string"
+      },
+      favoriteAnimal: {
+        type: "string"
+      }
+    }
+  }
+})
+
 export function createVillage (
   config: FullConfig = defaultConfig,
   backingServiceOverrides: Partial<BackingServices> = {}
 ): Village {
   const backingServices = createBackingServices(config, backingServiceOverrides)
-  const model = createModel(config.database)
+  const model = createModel(config.database, sampleSchema)
   const logic = {
     user: {}
   }
