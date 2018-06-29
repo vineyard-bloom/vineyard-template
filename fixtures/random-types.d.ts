8e3e388e2c3ffcc7724f98ecdc427fdf8678a865
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-06-29 10:02:07 -0600

    Cleaning up old files

--- fixtures/random-types.d.ts
+++
@@ -1,5 +0,0 @@
-import { Pizza } from '../src/model/model-types';
-import { CreatePizzaData } from '../src/logic/pizza-logic';
-import { Omit } from 'vineyard-data/modern';
-export declare function randomCreatePizzaData(): CreatePizzaData;
-export declare function randomPizzaToCreate(): Omit<Pizza, 'id'>;
--- src/endpoints/request-handlers/user-request-handlers.d.ts
+++ src/endpoints/request-handlers/user-request-handlers.d.ts
@@ -1,7 +1,6 @@
 import { requestTypes, responseTypes } from './api-types';
-import { UserLogic } from '../../logic/logic';
 export declare const userRequestHandler: {
     loginUser: (userService: any) => (req: requestTypes.LoginUserRequest) => Promise<responseTypes.LoginUserResponse>;
-    registerUser: (userLogic: UserLogic, userService: any) => (req: requestTypes.RegisterUserRequest) => Promise<responseTypes.RegisterUserResponse>;
+    registerUser: (userLogic: any, userService: any) => (req: requestTypes.RegisterUserRequest) => Promise<responseTypes.RegisterUserResponse>;
     logoutUser: (userService: any) => (req: requestTypes.LogoutUserRequest) => Promise<responseTypes.LogoutUserResponse>;
 };
